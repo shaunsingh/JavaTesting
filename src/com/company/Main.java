60bdd25da135a93e9c4dcb6b007e8005cfc073be
shaunsingh <shaunsingh0207@gmail.com>
2021-06-17 19:05:47 -0400

    added stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,5 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hi World");
-        System.out.println("Hi World");
     }
 }
